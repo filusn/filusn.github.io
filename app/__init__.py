1934e4d79a71d082290d9871c324194a74f03ba4
Filip Noworolnik <fnoworolnik@gmail.com>
2019-11-02 13:31:23 +0100

    update header and grid style

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,52 @@ def create_app():
     def home():
         return render_template('home.html')
 
-    @app.route('/blog/')
-    def blog():
-        return render_template('blog.html')
-
     @app.route('/projects/')
     def projects():
-        return render_template('projects.html')
+        projects = [
+            {
+                'title': 'ML fine art price prediction',
+                'body': 'Check if ML can predict the price of paintings.'
+            },
+            {
+                'title': 'Confocal microscopy',
+                'body': 'Tumour cell detection on confocal microscopy images'
+            },
+            {
+                'title': 'Deutsch Meister',
+                'body': 'Python and Starlette based app helping me and my friends to follow and repeat what have we learned.'
+            },
+            {
+                'title': 'QuantX Backtest',
+                'body': 'Backtesting engine for stock market.'
+            },
+            {
+                'title': 'Jigsaw Puzzle Solver',
+                'body': 'AI approach for solving jigsaw puzzles.'
+            }
+        ]
+        return render_template('projects.html', projects=projects)
+
+    @app.route('/blog/')
+    def blog():
+        posts = [
+            {
+                'title': 'pytest mini tutorial',
+                'date': '1.11.2019',
+                'body': 'This is mini pytest tutorial'
+            },
+            {
+                'title': 'ML Conference',
+                'date': '29.09.2019',
+                'body': 'I was on a conference...'
+            },
+            {
+                'title': 'HackYeah',
+                'date': '10.10.2019',
+                'body': 'Events on HackYeah...'
+            }
+        ]
+        return render_template('blog.html', posts=posts)
 
     @app.route('/cv/')
     def cv():
